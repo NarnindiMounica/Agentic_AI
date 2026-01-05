6fc2de89cc29c01d2b24c0587cc8a9213806fc87
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-01-05 23:22:14 +0530

    project 1: connectivity in main.py and app.py

--- Projects/agentic_chatbot/src/langgraphagenticai/main.py
+++ Projects/agentic_chatbot/src/langgraphagenticai/main.py
@@ -10,5 +10,17 @@ def load_langgraph_agenticai_app():
     This function initializes the UI, handles user input, configures the LLM Model, 
     sets up the graph based on the selected use case, and displays the output while
     implementing exception handling for robustness.
-    
+
     """
+
+    #load UI
+    ui=LoadStreamlitUI()
+    user_input=ui.load_streamlit_ui()
+
+    if not user_input:
+        st.error("Error: Failed to load user input from the UI.")
+        return
+    
+    user_message = st.chat_input("Enter your message: ")
+
+
