c6872c1fb13b6a5504e6e9f8718a2211acbd06bc
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-01-06 00:07:50 +0530

    project 1: agentic chatbot - defining graph workflow

--- Projects/agentic_chatbot/src/langgraphagenticai/graphs/graph_builder.py
+++ Projects/agentic_chatbot/src/langgraphagenticai/graphs/graph_builder.py
@@ -1,5 +1,27 @@
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, START, END
+from src.langgraphagenticai.states.state import State
 
 class GraphBuilder:
-    def __init__(self, ):
-        pass
+    def __init__(self ):
+        self.graph_builder = StateGraph(State)
+
+    def basic_chatbot_build_graph(self):
+        """
+        Docstring for basic_chatbot_build_graph
+        
+        Builds a basic chatbot graph using Langgraph.
+        This method initializes a chatbot node using the 'BasicChatbotNode' class
+        and integrates it into the graph. The chatbot node is set as both the entry
+        and exit point of the graph.
+
+        """   
+        self.graph_builder.add_node("basic_chatbot", ) 
+
+        self.graph_builder.add_edge(START, "basic_chatbot")
+        self.graph_builder.add_edge("basic_chatbot", END)
+
+        graph = self.graph_builder.compile()
+
+        return graph
+
+        
