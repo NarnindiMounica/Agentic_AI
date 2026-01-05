89d7cc9b9a9c80bd35d7c96b7de9ac9df8f74f9f
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-01-06 00:24:07 +0530

    project 1: agentic chatbot - defining basic chatbot node functionality

---
+++ Projects/agentic_chatbot/src/langgraphagenticai/nodes/basic_chatbot_node.py
@@ -0,0 +1,21 @@
+import os
+import streamlit as st
+
+from src.langgraphagenticai.llms.groqllm import GroqLLM
+from src.langgraphagenticai.states.state import State
+
+class BasicChatbotNode:
+
+    def __init__(self, model):
+        self.llm =model
+
+    def basic_chatbot_node(self, state:State)->dict:
+        """
+        Docstring for basic_chatbot_node
+        
+        Process the input state and generates a chatbot response
+        """
+
+        response = self.llm.invoke(state['messages']) 
+
+        return {"messages": response}  
