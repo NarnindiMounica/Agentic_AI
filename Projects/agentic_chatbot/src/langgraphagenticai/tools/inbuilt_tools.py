8b1197c104febfad39daafabc6551251b0c7fe43
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-01-08 03:26:58 +0530

    adding inbuilt tools and toolnode

---
+++ Projects/agentic_chatbot/src/langgraphagenticai/tools/inbuilt_tools.py
@@ -0,0 +1,32 @@
+from langchain_tavily import TavilySearch
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+
+from langchain.prebuilt import ToolNode
+
+
+class InbuiltTools:
+
+    def __init__(self):
+        pass
+
+    def get_inbuilt_tools(self):
+
+        "this function returns a list of defined inbuilt tools"
+        
+        tavily_tool = TavilySearch(max_results=2)
+        wikipedia_tool = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+
+        given_tools = [tavily_tool, wikipedia_tool]
+
+        return given_tools
+    
+    def get_tool_node(self, tool):
+
+        "This function creates a tool node with the list of given tools"
+
+        return ToolNode(tool)
+
+
+
+
