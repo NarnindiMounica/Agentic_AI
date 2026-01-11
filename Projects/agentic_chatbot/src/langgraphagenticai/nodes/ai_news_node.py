c3b17423c4c62385978da17e2a2ea8a9189811d2
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-01-11 23:50:40 +0530

    ai_news nodes defintion

--- Projects/agentic_chatbot/src/langgraphagenticai/nodes/ai_news_node.py
+++ Projects/agentic_chatbot/src/langgraphagenticai/nodes/ai_news_node.py
@@ -32,9 +32,13 @@ class AINewsNode:
 
         response = self.tavily.search(query="Get Latest News in the field of Artificial Intelligence from India and globally",
                                       topic="news",
-                                      time_range=self.state['frequency'],
+                                      time_range=time_range_map[frequency],
                                       max_results=3,
-                                      include_images=True)
+                                      include_answer="advanced",
+                                      days=days_map[frequency])
         
+        state['news_data'] = response.get("results", [])
+        self.state['news_data'] = state['news_data']  
+        return state      
 
 
