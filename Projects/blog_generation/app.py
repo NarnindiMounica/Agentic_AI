763fe586801c51cf4370631888fe4c70466e3c82
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-01-15 03:23:03 +0530

    adding usecase in app.py

--- Projects/blog_generation/app.py
+++ Projects/blog_generation/app.py
@@ -40,7 +40,7 @@ async def create_blogs(request:Request):
 
 
 if __name__=="__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app",host="0.0.0.0", port=8000, reload=True)
 
 
 
